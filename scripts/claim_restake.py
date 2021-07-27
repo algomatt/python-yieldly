5f4a92b2d01e80dfd3ad750cc1b6a6d46ced5a88
Matt <87902651+algomatt@users.noreply.github.com>
2021-07-27 17:39:57 +0000

    Fixed exception if either lottery or staking has no claimable rewards

--- scripts/claim_restake.py
+++ scripts/claim_restake.py
@@ -3,15 +3,21 @@
 from yieldly.operations import lottery, staking
 from yieldly.util import post_slack_message
 
+claimed = 0
+
 lottery_claim = lottery.ClaimOperation()
-lottery_claim.send()
+
+if lottery_claim.claimable:
+    lottery_claim.send()
+    claimed += lottery_claim.claimable
 
 staking_claim = staking.ClaimOperation()
-staking_claim.send()
 
-claimed = lottery_claim.claimable + staking_claim.claimable["yldy"]
+if staking_claim.claimable["yldy"]:
+    staking_claim.send()
+    claimed += staking_claim.claimable["yldy"]
 
 stake = staking.StakeOperation(amount=claimed)
 stake.send()
 
-post_slack_message(f'Claim & restake of {claimed / 1e6:.6} YLDY complete.')
+post_slack_message(f"Claim & restake of {claimed / 1e6:.6} YLDY complete.")
